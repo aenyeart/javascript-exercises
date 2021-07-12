7308df693c8b8da19d9726ffacf3653e3cbab324
aenyeart <reallyhardtoforget@gmail.com>
2021-07-12 14:21:04 -0700

    Solves removeFromArray exercise

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,21 +1,22 @@
-const removeFromArray = function(sourceArray, ...peskyNums) {
-
-//Iterate through sourceArray
+const removeFromArray = function(sourceArray, ...peskyArgs) {
   for (i = 0; i < sourceArray.length; i++) {
-    //For each element, iterate through peskyNums array and compare values
-    for (j = 0; j < peskyNums.length; j++){
-      //if they match, splice() pesky number out of sourceArray
-      if (sourceArray[i] == peskyNums[j]) sourceArray.splice(i, 1); // this causes the sourceArray to shift and "miss" the next element (it now has the same index as the element that was just iterated over and removed)
-        // Could solve by:
-          // creating new array with the values that are NOT targeted for removal, leaving sourceArray intact
-          // trying .forEach() to see if it handles this
-
-          
-          }
-      else continue;
+    for (j = 0; j < peskyArgs.length; j++){
+      while (sourceArray[i] === peskyArgs[j]) { // while accounts for shifting indexes when element is spliced out
+        sourceArray.splice(i, 1);
+        j = 0; // resetting counter post-splice ensures ALL arguments are compared
+      }
     }
   }
   return sourceArray;
 };
 
 module.exports = removeFromArray;
+
+//Iterate through sourceArray
+//For each element, iterate through peskyArgs to compare values
+//if they match, splice() that element out of sourceArray
+
+// The part that is challenging: splice() causes the sourceArray to shift and iteration  "misses" the next element (as the next element now has the same index as the element that was just removed)
+  // Ideas:
+    // creating new array with the values that are NOT targeted for removal, leaving sourceArray intact
+    // trying .forEach() to see if it handles this

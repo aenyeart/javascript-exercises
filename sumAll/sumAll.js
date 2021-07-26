5857fc4db2ec4089a2d5cd04ec9918ecccdbdb83
aenyeart <reallyhardtoforget@gmail.com>
2021-07-26 15:18:59 -0700

    Adds first working solution to sumAll exercise

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(...nums) {
+  nums.sort( (a,b) => a - b ); // puts in ascending order
+  if (nums.some(num => num < 0) ||
+    nums.some(num => typeof(num) !== 'number')) {
+      return 'ERROR';
+  }
+  let a = nums[0];
+  let b = nums[1];
+  let sum = 0
+  for (; a <= b; a++) sum += a;
+  return sum;
 };
 
 module.exports = sumAll;

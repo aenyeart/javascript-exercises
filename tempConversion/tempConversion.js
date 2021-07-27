78791bd705d34bb58fedeb4f04a8b8fbdfae7e20
aenyeart <reallyhardtoforget@gmail.com>
2021-07-26 17:07:28 -0700

    Adds solution for tempConversion exercise

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temperature) {
+  let celsius = (temperature - 32)/1.8;
+  let nearest10th = Math.round(celsius * 10)/10;
+  return nearest10th;
 };
 
-const ctof = function() {
-
+const ctof = function(temperature) {
+  let fahrenheit = (temperature * 1.8) + 32;
+  let nearest10th = Math.round(fahrenheit * 10)/10;
+  return nearest10th;
 };
 
 module.exports = {

fa0a0bcea0612d0d2a750273899f80825541cf39
aenyeart <reallyhardtoforget@gmail.com>
2021-07-26 17:16:19 -0700

    Condenses IF statements in leapYears function to one line

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,8 +1,15 @@
-const leapYears = function(year) {
-  if (year % 400 == 0) return true;
-  if (year % 100 == 0) return false;
-  if (year % 4 ==0) return true;
-  return false;
-};
+// FIRST SOLUTION:
 
+// const leapYears = function(year) {
+//   if (year % 400 == 0) return true;
+//   if (year % 100 == 0) return false;
+//   if (year % 4 ==0) return true;
+//   return false;
+// };
+
+
+// REVISED SOLUTION:
+const leapYears = function(year) {
+  return (year % 400 === 0) || (year % 4 ===0 && year % 100 !== 0);
+}
 module.exports = leapYears;
